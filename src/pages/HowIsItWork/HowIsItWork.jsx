06aaf6ee3c68795d2407ca152a4128ecdbeb0221
PranayChowdhury00 <pranaychowdhury00@gmail.com>
2025-04-02 12:34:09 +0600

    completeing the seconde section  for how is it work

--- src/pages/HowIsItWork/HowIsItWork.jsx
+++ src/pages/HowIsItWork/HowIsItWork.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import FirstSection from '../FirstSection/FirstSection';
+import SecondeSection from './SecondeSection/SecondeSection';
+
 
 const HowIsItWork = () => {
     return (
         <div >
             <Banner></Banner>
             <FirstSection></FirstSection>
+         <SecondeSection></SecondeSection>
         </div>
     );
 };
